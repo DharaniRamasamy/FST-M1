5e00b8099166ea4421aa96d93f3540b6981af341
DharaniRamasamy <57927769+DharaniRamasamy@users.noreply.github.com>
2021-05-13 10:09:02 +0530

    Add files via upload

---
+++ python/Activity21_test.py
@@ -0,0 +1,25 @@
+import pytest
+
+def test_sum():
+    num1 = 10
+    num2=30
+    sum=num1+num2
+    assert sum==40
+
+def test_diff():
+    num1=50
+    num2=30
+    diff=num1-num2
+    assert diff==20
+
+def test_prod():
+    num1=10
+    num2=30
+    prod=num1*num2
+    assert prod==300
+
+def test_quo():
+    num1=100
+    num2=5
+    quo=num1/num2
+    assert quo==20
